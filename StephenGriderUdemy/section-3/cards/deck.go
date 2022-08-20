af261badc0d207811a3ce8e7df4744a40b6ae7a3
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-08-20 10:28:18 -0400

    newDeck and receiver

---
+++ StephenGriderUdemy/section-3/cards/deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+
+	for _, suit := range cardSuits { //because we don't want to use the index
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {  //receiver
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
