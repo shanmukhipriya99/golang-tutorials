4249cb87384b1f9928d7121ff0ca49a3c74916ec
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-08-20 17:02:33 -0400

    reading files from system

--- StephenGriderUdemy/section-3/cards/deck.go
+++ StephenGriderUdemy/section-3/cards/deck.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 	"io/ioutil"
+	"os"
 )
 
 // Create a new type of 'deck'
@@ -40,4 +41,15 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1) //non-zero=>error
+	}
+
+	s := strings.Split(string(bs), ",") //returns slice of strings
+	return deck(s)
 }
--- StephenGriderUdemy/section-3/cards/main.go
+++ StephenGriderUdemy/section-3/cards/main.go
@@ -3,14 +3,15 @@ package main
 // import "fmt"
 
 func main() {
-	cards := newDeck()
-
+	// cards := newDeck()
+	cards := newDeckFromFile("myCards")
 	// hand, remainingDeck := deal(cards, 5)
 	// hand.print()
 	// remainingDeck.print()
-	// cards.print()
+	cards.print()
 	// greeting := "Hey!"
 	// fmt.Println([]byte(greeting))
 	// fmt.Println(cards.toString())
-	cards.saveToFile("myCards")
+	// cards.saveToFile("myCards")
+
 }
