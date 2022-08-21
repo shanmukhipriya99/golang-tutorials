957ebe8da106db8fa6f12cfb4ff8476ab13f3377
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-08-21 11:58:15 -0400

    embedding structs

--- StephenGriderUdemy/section-4/main.go
+++ StephenGriderUdemy/section-4/main.go
@@ -2,17 +2,33 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zip int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
-	newPerson := person{firstName: "abc", lastName: "def"}
-	// newPerson := person{"abc", "def"} //order needs to be maintained
-	fmt.Println(newPerson)
-	fmt.Printf("%+v", newPerson) 
-	//returns
-	//{abc def}
-	// {firstName:abc lastName:def}%                          
+	// newPerson := person{firstName: "abc", lastName: "def"}
+	// // newPerson := person{"abc", "def"} //order needs to be maintained
+	// fmt.Println(newPerson)
+	// fmt.Printf("%+v", newPerson) 
+	// //returns
+	// //{abc def}
+	// // {firstName:abc lastName:def}% 
+	
+	newPerson := person{
+		firstName: "abc", 
+		lastName: "def", 
+		contact: contactInfo{
+			email: "abc@gmail.com", 
+			zip: 12345,
+			},
+	}
+	fmt.Printf("%+v", newPerson)
 }
