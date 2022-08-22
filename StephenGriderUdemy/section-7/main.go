a5e64f973d46e837151369132ca4e574d23a6424
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-08-22 17:58:41 -0400

    printing site status

---
+++ StephenGriderUdemy/section-7/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://golang.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	} else {
+		fmt.Println(link, "is up!")
+	}
+}
