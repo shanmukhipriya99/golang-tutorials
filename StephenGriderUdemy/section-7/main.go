9284e14280418decf780079b5c12adf223b776f0
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-08-22 20:37:09 -0400

    channels & routines

--- StephenGriderUdemy/section-7/main.go
+++ StephenGriderUdemy/section-7/main.go
@@ -11,17 +11,33 @@ func main() {
 		"http://golang.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c) //go => creates a new thread go routine
+		// fmt.Println(<-c)
 	}
+
+	// for {  //infinite loop
+	// 	// fmt.Println(<-c)
+	// 	go checkLink(<-c, c)
+	// }
+
+	for l := range c {  //infinite loop
+		// fmt.Println(<-c)
+		go checkLink(l, c)
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	} else {
 		fmt.Println(link, "is up!")
+		c <- link
 	}
 }
